05868e6094d10497990a78a7c2f3010d0b11a034
Ashok Guduru <gw2ksoft@hotmail.com>
2017-06-25 11:13:32 +0530

    Added missing classes for Foresight

---
+++ Foresight.Win/Common/ApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿//using Scalable.Shared.Common;
+//using Scalable.Win.Common;
+
+//namespace Foresight.Win.Common
+//{
+//    public static class ApplicationFactory
+//    {
+//        public static ScalableApplicationBase Instance()
+//        {
+//#if DEBUG
+//            if (AppConfig.UseTestCoGroup)
+//            {
+//                return new TestCoGroupFerryApp();
+//            }
+//#endif
+//            return new ForesightApp();
+//        }
+//    }
+//}
---
+++ Foresight.Win/Common/ForesightApp.cs
@@ -0,0 +1,13 @@
+﻿//using Gravity.Root.Common;
+//using Gravity.Root.Forms;
+
+//namespace Foresight.Win.Common
+//{
+//    public class ForesightApp : ReleaseGravityAppBase
+//    {
+//        protected override FMainBase getMainForm()
+//        {
+//            return new FMain(this);
+//        }
+//    }
+//}
